d04b227fb3aa2ad84cfce421966b14374310245f
Valeria-CPaz <val.paz@gmail.com>
2025-04-07 14:43:29 -0300

    desafio01 dev superior

---
+++ src/main/java/com/devsuperior/desafio1/services/OrderService.java
@@ -0,0 +1,17 @@
+package com.devsuperior.desafio1.services;
+
+import com.devsuperior.desafio1.entities.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    ShippingService shippingService;
+
+    public double total(Order order){
+        double discount = order.getBasic() * order.getDiscount() / 100;
+        return order.getBasic() - discount + shippingService.shipment(order);
+    }
+}
